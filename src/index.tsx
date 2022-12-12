b95a64c5c90ae5c23ca00cf00f3d9586d4d7cfc8
Aheuf <clement.genty@gmail.com>
2022-12-12 21:37:01 +0100

    fix: context

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PlayerProvider from './context/PlayerContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <PlayerProvider origin={''}>
+      <App />
+    </PlayerProvider>
   </React.StrictMode>
 );
 
